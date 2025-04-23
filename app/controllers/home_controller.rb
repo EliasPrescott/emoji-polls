cf3847a6d93d20ae55488c879b456a0108aeacfd
eliasprescott <syzygyprescott@gmail.com>
2025-04-23 07:55:33 -0500

    adding the featured polls list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 
   def index
     if authenticated?
+      @featured_polls = Poll.where(id: [ 1, 2 ])
       @polls = Current.session.user.polls.order(created_at: :desc)
     else
       @polls = nil
